74b2dc4c9f0464a1d6ef7096f7acf8ea7c6ab8a5
Data-drone <bpl.law@gmail.com>
2017-11-29 00:58:48 +0800

    added multiple embedding objects and got model running

--- Embedding_Model_1.py
+++ Embedding_Model_1.py
@@ -6,20 +6,39 @@ import numpy as np
 # keras libs
 from keras.models import Model
 from keras.layers import Dense, Flatten, Dropout, Input, Embedding, Reshape
+from keras.layers import concatenate
 
 data_path = 'data'
 
 train_set = pd.read_csv(os.path.join(data_path, 'train.csv'))
 
-Input_X = np.array(train_set.Store)
+Input_Store = np.array(train_set.Store)
+Input_Dow = np.array(train_set.DayOfWeek)
+
+
 Input_Y = np.array(train_set.Sales)
 
 # keras model
-model_in = Input(shape=(1,))
-embedding_1 = Embedding(1115, 50, input_length = 1)(model_in)
-res = Reshape(target_shape=(50,))(embedding_1)
-dense_1 = Dense(1)(res)
-model = Model(inputs = model_in, outputs = dense_1)
+
+# store in
+store_in = Input(shape=(1,))
+embedding_1 = Embedding(1115, 50, input_length = 1)(store_in)
+res_store = Reshape(target_shape=(50,))(embedding_1)
+
+# day of week in
+dow_in = Input(shape=(1,))
+embedding_2 = Embedding(7, 6, input_length = 1)(dow_in)
+res_dow = Reshape(target_shape=(6,))(embedding_2)
+
+# main
+Merger = concatenate([res_store, res_dow])
+drop_1 = Dropout(0.2)(Merger)
+dense_1 = Dense(64)(drop_1)
+drop_2 = Dropout(0.2)(dense_1)
+
+dense_out = Dense(1)(drop_2)
+
+model = Model(inputs = [store_in, dow_in], outputs = dense_out)
 model.compile(loss='mae', optimizer='adam')
 
-model.fit(Input_X, Input_Y, epochs = 10, batch_size = 10)
+model.fit([Input_Store, Input_Dow], Input_Y, epochs = 10, batch_size = 10)

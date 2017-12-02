40fa7d91a0612dd3cd2c8eb26fc47150428cc24c
Data-drone <bpl.law@gmail.com>
2017-12-02 20:51:34 +0800

    commenting n stuff

--- Embedding_Model_1.py
+++ Embedding_Model_1.py
@@ -15,6 +15,9 @@ train_set = pd.read_csv(os.path.join(data_path, 'train.csv'))
 Input_Store = np.array(train_set.Store)
 Input_Dow = np.array(train_set.DayOfWeek)
 
+Input_Year = np.array(train_set.Date.dt.year)
+Input_Month = np.array(train_set.Date.dt.month)
+Input_Day = np.array(train_set.Date.dt.day)
 
 Input_Y = np.array(train_set.Sales)
 
@@ -30,15 +33,32 @@ dow_in = Input(shape=(1,))
 embedding_2 = Embedding(7, 6, input_length = 1)(dow_in)
 res_dow = Reshape(target_shape=(6,))(embedding_2)
 
+# year
+year_in = Input(shape = (1,))
+embedding_yr = Embedding(3, 2, input_length = 1)(year_in)
+res_yr = Reshape(target_shape=(2,))(embedding_yr)
+
+# month
+month_in = Input(shape = (1,))
+embedding_month = Embedding(12, 6, input_length = 1)(month_in)
+res_month = Reshape(target_shape=(6,))(embedding_month)
+
+# day
+day_in = Input(shape = (1,))
+embedding_day = Embedding(31, 10, input_length = 1)(day_in)
+res_day = Reshape(target_shape=(10,))(embedding_day)
+
 # main
-Merger = concatenate([res_store, res_dow])
+Merger = concatenate([res_store, res_dow, res_yr, res_month, res_day])
 drop_1 = Dropout(0.2)(Merger)
-dense_1 = Dense(64)(drop_1)
+dense_1 = Dense(100)(drop_1)
 drop_2 = Dropout(0.2)(dense_1)
 
 dense_out = Dense(1)(drop_2)
 
-model = Model(inputs = [store_in, dow_in], outputs = dense_out)
+model = Model(inputs = [store_in, dow_in, year_in, month_in, day_in], outputs = dense_out)
 model.compile(loss='mae', optimizer='adam')
 
-model.fit([Input_Store, Input_Dow], Input_Y, epochs = 10, batch_size = 10)
+result = model.fit([Input_Store, Input_Dow, Input_Year, Input_Month, Input_Day], Input_Y, epochs = 15, batch_size = 128)
+
+model.save('model/embedding_2.h5')
--- LSTM_Model_6.py
+++ LSTM_Model_6.py
@@ -1,5 +1,5 @@
 
-# load
+# load data
 import pickle
 X_train = pickle.load(open( 'feat_table/xgb_1_x_train_table.pkl', "rb" ))
 y_train = pickle.load(open( 'feat_table/xgb_1_y_train_table.pkl', "rb" ))
@@ -8,26 +8,26 @@ y_valid = pickle.load(open( 'feat_table/xgb_1_y_valid_table.pkl', "rb" ))
 test = pickle.load(open( 'feat_table/xgb_1_test_table.pkl', "rb" ))
 features = pickle.load(open( 'feat_table/xgb_1_features_vector_vector.pkl', "rb" ))
 
-# keras libs
+# import keras libs
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Flatten, Dropout
 import numpy as np
 
-# reformatting
+# reformatting the datasets
 x_train_array = np.copy(X_train[features].values)
 x_valid_array = np.copy(X_valid[features].values)
 
 format_sub_train_x = x_train_array.reshape((x_train_array.shape[0], 1, x_train_array.shape[1]) )
 format_sub_val_x = x_valid_array.reshape((x_valid_array.shape[0], 1, x_valid_array.shape[1]) )
 
-# custom loss function
+# custom loss function for the rossman comp
 from keras import backend as K
 def mean_squared_percentage_error(y_true, y_pred):
     return K.mean(K.square((y_true - y_pred) / K.clip(K.abs(y_true),
                                             K.epsilon(),
                                             None)), axis=-1)
-# mspe = MSPE = mean_squared_percentage_error
 
+# model definition (put elsewhere?)
 model_input = Input( shape = (format_sub_train_x.shape[1], format_sub_train_x.shape[2]) )
 lstm_1 = LSTM(100, return_sequences=True)(model_input)
 dropout_mod = Dropout(0.2)(lstm_1)
@@ -40,16 +40,17 @@ model = Model(inputs = model_input, outputs = output)
 model.compile(loss=mean_squared_percentage_error, optimizer='adam')
 model.save('model/LSTM_Model_6.h5')
 
-# model 5 training
-mod_5 = model.fit(format_sub_train_x, np.copy(y_train), epochs=350, batch_size=100, 
+# model 6 training
+mod_6 = model.fit(format_sub_train_x, np.copy(y_train), epochs=350, batch_size=100, 
                     validation_data=(format_sub_val_x, np.copy(y_valid)), verbose=2, shuffle=False)
 
 dtest = np.copy(test[features].values)
 test_frame = dtest.reshape((dtest.shape[0], 1, dtest.shape[1]) )
 
 test_probs = model.predict(test_frame)
-# Make Submission
 
+
+# Make Submission file
 import pandas as pd
 result = pd.DataFrame({"Id": test["Id"], 'Sales': np.expm1(test_probs.flatten())})
 
